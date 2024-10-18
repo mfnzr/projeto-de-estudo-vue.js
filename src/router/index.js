644817a26087cc640305faeab40a0696133a6288
Maria Fernanda <mfnanda.ribeiro@gmail.com>
2024-10-17 21:59:01 -0300

    projeto video aulas vue.js

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: {
+        title: 'Home'
+      }
+    },
+    {
+      path: '/pedidos',
+      name: 'Pedidos',
+      component: () => import('../views/Pedidos.vue'),
+      meta: {
+        title: 'About'
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router
