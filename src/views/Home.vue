5f59ac8659983e4c344807187680ff89acedfd29
Maria Fernanda <mfnanda.ribeiro@gmail.com>
2024-10-18 16:36:39 -0300

    formulário de burguer implementado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
   <Banner />
-  <div class="main-container">
-    <h1>Monte o seu burguer:</h1>
-    <p>formulário</p>
-  </div>
+    <div class="main-container">
+      <h1>Monte o seu burguer:</h1>
+      <BurguerForm />
+    </div>
 </div>
 </template>
 
 <script>
 import Banner from '../components/Banner.vue';
+import BurguerForm from '@/components/BurguerForm.vue';
 
 export default {
   name: "Home",
   components: {
-    Banner
+    Banner,
+    BurguerForm
   }
 }
 </script>
